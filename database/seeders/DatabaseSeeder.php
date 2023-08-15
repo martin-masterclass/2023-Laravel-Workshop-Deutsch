021a252c8571c1772e04861f5f64b9e2ad4c196f
Martin Krebs Eberth <>
2023-08-15 11:15:37 +0200

    Verschiedene User-Typen seeden

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Carbon;
 
 class DatabaseSeeder extends Seeder
 {
@@ -12,11 +13,19 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
+        \App\Models\User::factory()->create([
+            'name' => 'Martin',
+            'email' => 'hello@laravelacademy.com',
+            'password' => bcrypt('bla123'),
+            'role' => 'admin'
+        ]);
+        $divisions = ['Kundenservice', 'Management', 'IT', 'Backoffice' ];
+        \App\Models\User::factory(50)->create([
+            'password' => bcrypt('x'),
+        ])->each(function ($user) use ($divisions) { $user->update([
+            'division' => $divisions[rand(0, count($divisions)-1)],
+            'birthday' => Carbon::now()->subDays(rand(20*365, 65*365)),
+            ]);
+        } );
     }
 }
